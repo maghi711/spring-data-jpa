7e881eed69162f2b8dc7f7d57aa575889d5fe761
Mahesh J <mahesh.jyothi.ramalingam@signify.com>
2022-01-27 21:03:33 +0530

    moved the repo package.

--- src/main/java/com/maghi711/springbootjpa/product/repos/ProductRepository.java
+++ src/main/java/com/maghi711/springbootjpa/product/repos/ProductRepository.java
@@ -1,5 +1,6 @@
-package com.maghi711.springbootjpa.product.entities;
+package com.maghi711.springbootjpa.product.repos;
 
+import com.maghi711.springbootjpa.product.entities.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ProductRepository extends JpaRepository<Product, Integer> {
