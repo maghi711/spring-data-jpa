8c6bf5bdded32e2d704d6eb69e1e5c42522516ea
Mahesh J <mahesh.jyothi.ramalingam@signify.com>
2022-01-27 22:06:23 +0530

    testing create logic. also added show sql in configuration.

--- src/test/java/com/maghi711/bootsecuritydemo/SpringBootJpaApplicationTests.java
+++ src/test/java/com/maghi711/bootsecuritydemo/SpringBootJpaApplicationTests.java
@@ -1,7 +1,10 @@
 package com.maghi711.bootsecuritydemo;
 
+import com.maghi711.springbootjpa.product.entities.Product;
+import com.maghi711.springbootjpa.product.repos.ProductRepository;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
@@ -9,8 +12,27 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 @SpringBootTest
 class SpringBootJpaApplicationTests {
 
+	@Autowired
+	ProductRepository productRepository;
+
 	@Test
 	void contextLoads() {
 	}
 
+	@Test
+	void testCreate() {
+		Product product = new Product();
+		product.setId(2L);
+		product.setName("Vivo z1Pro");
+		product.setDesc("Online only release phone");
+		product.setPrice(16000.000);
+
+		productRepository.save(product);
+		/**
+		 * 2022-01-27 22:04:56.103  INFO 13035 --- [    Test worker] c.m.b.SpringBootJpaApplicationTests      : Started SpringBootJpaApplicationTests in 3.981 seconds (JVM running for 5.375)
+		 * Hibernate: select product0_.id as id1_0_0_, product0_.description as descript2_0_0_, product0_.name as name3_0_0_, product0_.price as price4_0_0_ from product product0_ where product0_.id=?
+		 * Hibernate: insert into product (description, name, price, id) values (?, ?, ?, ?)
+		 * 2022-01-27 22:04:56.336  INFO 13035 --- [ionShutdownHook] j.LocalContainerEntityManagerFactoryBean : Closing JPA EntityManagerFactory for persistence unit 'default'
+		 */
+	}
 }
