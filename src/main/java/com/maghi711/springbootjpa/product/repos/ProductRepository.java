9e07fde35419dde05879f390d205cdb9e40540fd
Mahesh J <mahesh.jyothi.ramalingam@signify.com>
2022-01-27 22:11:50 +0530

    changed the primary key to long from integer

--- src/main/java/com/maghi711/springbootjpa/product/repos/ProductRepository.java
+++ src/main/java/com/maghi711/springbootjpa/product/repos/ProductRepository.java
@@ -3,6 +3,6 @@ package com.maghi711.springbootjpa.product.repos;
 import com.maghi711.springbootjpa.product.entities.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ProductRepository extends JpaRepository<Product, Integer> {
+public interface ProductRepository extends JpaRepository<Product, Long> {
 
 }
