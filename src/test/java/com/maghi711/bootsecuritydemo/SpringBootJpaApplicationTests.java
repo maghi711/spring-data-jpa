9f5e19c512ef6e708e481d53ec7bcb0f0e4022b7
Mahesh J <mahesh.jyothi.ramalingam@signify.com>
2022-01-27 22:17:15 +0530

    added read test case

--- src/test/java/com/maghi711/bootsecuritydemo/SpringBootJpaApplicationTests.java
+++ src/test/java/com/maghi711/bootsecuritydemo/SpringBootJpaApplicationTests.java
@@ -2,11 +2,17 @@ package com.maghi711.bootsecuritydemo;
 
 import com.maghi711.springbootjpa.product.entities.Product;
 import com.maghi711.springbootjpa.product.repos.ProductRepository;
+import org.assertj.core.api.Assert;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest
@@ -34,5 +40,13 @@ class SpringBootJpaApplicationTests {
 		 * Hibernate: insert into product (description, name, price, id) values (?, ?, ?, ?)
 		 * 2022-01-27 22:04:56.336  INFO 13035 --- [ionShutdownHook] j.LocalContainerEntityManagerFactoryBean : Closing JPA EntityManagerFactory for persistence unit 'default'
 		 */
+	}
+
+	@Test
+	void testRead() {
+		final Optional<Product> byId = productRepository.findById(1L);
+		assertThat(byId).isNotNull();
+		final Product actual = byId.get();
+		assertThat(actual.getName()).isEqualTo("Mi A1");
 	}
 }
