5356ea487b542508b609f821dee6deca72628532
Mahesh J <mahesh.jyothi.ramalingam@signify.com>
2022-01-28 15:58:27 +0530

    define spring repository

---
+++ src/main/java/com/maghi711/associations/repos/EmployeeRepository.java
@@ -0,0 +1,10 @@
+package com.maghi711.associations.repos;
+
+import com.maghi711.associations.entities.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+
+}
